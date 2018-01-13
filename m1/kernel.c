8a406f918e6d8a2e69f3736f25f7092aecbd975c
lewistd <lewistd@rose-hulman.edu>
2018-01-13 15:37:38 -0500

    linted and modified hello

--- m1/kernel.c
+++ m1/kernel.c
@@ -4,26 +4,16 @@
 #define QEMU_OFFSET 80*9*2
 #define VMEM_WRITE_ARG1 0xB000
 #define VMEM_WRITE_ARG2 0x8000+QEMU_OFFSET
-int main(){
+int main() {
   int i;
-  char hello[13];
-  hello[0] = 'H';
-  hello[1] = 'e';
-  hello[2] = 'l';
-  hello[3] = 'l';
-  hello[4] = 'o';
-  hello[5] = ' ';
-  hello[6] = 'W';
-  hello[7] = 'o';
-  hello[8] = 'r';
-  hello[9] = 'l';
-  hello[10] = 'd';
-  hello[11] = '!';
-   
-  /*char hello[13] = {'H', 'e', 'l', lo World!";*/
-  for (i = 0; i < 12; i++){
+  char *hello;
+  hello = "Hello World!";
+
+  for (i = 0; i < 12; i++) {
     putInMemory(VMEM_WRITE_ARG1, VMEM_WRITE_ARG2 + i*2, hello[i]);
     putInMemory(VMEM_WRITE_ARG1, VMEM_WRITE_ARG2 + i*2 +1, 0x7);
   }
-  while(1);
+
+  while (1)
+    continue;
 }
