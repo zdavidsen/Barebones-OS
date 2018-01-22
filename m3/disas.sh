a06241325735f555f6a8c7b2d3646185ac07c4c9
Zac <davidszt@rose-hulman.edu>
2018-01-22 00:04:11 -0500

    fixed conditional bug in disas.sh

--- m3/disas.sh
+++ m3/disas.sh
@@ -1,6 +1,6 @@
 base="objdump -b binary --architecture=i386 -D -M intel floppya.img"
 
-if [ $# -l 1 ]; then
+if [ $# -lt 1 ]; then
     echo "Usage: $0 [-pDir] | [sector [additional sectors]]"
 elif [ "$1" == "-pDir" ]; then
     hexdump -C -n 512 -s 1024 floppya.img
