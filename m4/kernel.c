73b519d0dc75b2b5ecb1e042fcce5b1bd6bfefab
Chris <nurrencd@rose-hulman.edu>
2018-01-24 09:58:29 -0500

    added deleteFile and writeSector

---
+++ m4/kernel.c
@@ -0,0 +1,302 @@
+/* Written by (Team 02) Chris Nurrenberg, Zac Davidsen, Trey Lewis 1/20/18*/
+
+void printString(char* str);
+void readString(char* line);
+void readSector(char* buffer, int sector);
+int readFile(char *name, char *buffer);
+void executeProgram(char *name, int segment);
+void terminate();
+void writeSector(char *name, int segment);
+void deleteFile(char *name);
+
+int main() {
+  char buffer[13312];
+  makeInterrupt21();
+
+  interrupt(0x21, 4, "shell", 0x2000, 0);
+
+  /*interrupt(0x21, 3, "messag", buffer, 0);
+  interrupt(0x21, 0, buffer, 0, 0);
+
+  interrupt(0x21, 4, "tstpr2", 0x2000, 0);*/
+
+  while (1)
+    continue;
+
+  return 0;
+}
+
+void handleInterrupt21(int ax, int bx, int cx, int dx) {
+  char error[22];
+  error[0] = 'I';
+  error[1] = 'n';
+  error[2] = 'c';
+  error[3] = 'o';
+  error[4] = 'r';
+  error[5] = 'r';
+  error[6] = 'e';
+  error[7] = 'c';
+  error[8] = 't';
+  error[9] = ' ';
+  error[10] = 'a';
+  error[11] = 'x';
+  error[12] = ' ';
+  error[13] = 'v';
+  error[14] = 'a';
+  error[15] = 'l';
+  error[16] = 'u';
+  error[17] = 'e';
+  error[18] = '.';
+  error[19] = '\n';
+  error[20] = '\r';
+  error[21] = '\0';
+
+  switch (ax) {
+  case 0:
+    printString(bx);
+    break;
+  case 1:
+    readString(bx);
+    break;
+  case 2:
+    readSector(bx, cx);
+    break;
+  case 3:
+    readFile(bx, cx);
+    break;
+  case 4:
+    executeProgram(bx, cx);
+    break;
+  case 5:
+    terminate();
+    break;
+  case 6:
+    writeSector(bx, cx);
+    break;
+  case 7:
+    deleteFile(bx);
+    break;
+  default:
+    printString(error);
+  }
+}
+
+int div(int a, int b) {
+  int count;
+  count = 0;
+  while (a >= b) {
+    a = a-b;
+    count++;
+  }
+  return count;
+}
+
+int mod(int a, int b) {
+  while (a >= b) {
+    a = a-b;
+  }
+  return a;
+}
+
+void printString(char *str) {
+  char al, ah;
+  int ax, i;
+
+  for (i = 0;; i++) {
+    al = str[i];
+    if (al == 0)
+      break;
+    ah = 0xE;
+    ax = ah * 256 + al;
+    interrupt(0x10, ax, 0, 0, 0);
+  }
+}
+
+void readString(char *line) {
+  char temp, al, ah;
+  int ax, i;
+  i = 0;
+  while (1) {
+  ax = 0;
+  temp = interrupt(0x16, ax, 0, 0, 0);
+  if (temp == 0xd) {
+    line[i] = 0;
+    /* line[i] = 0xa;
+    line[i+1] = 0xd;
+    line[i+2] = 0x0; */
+    ax = 0xE * 256 + 0xa;
+    interrupt(0x10, ax, 0, 0, 0);
+    ax = 0xE * 256 + 0xd;
+    interrupt(0x10, ax, 0, 0, 0);
+    return;
+  } else if (temp == 0x8) {
+    if ( i > 0 ) {
+      i--;
+      ax = 0xE * 256 + 0x8;
+      interrupt(0x10, ax, 0, 0, 0);
+      ax = 0xE * 256 + ' ';
+      interrupt(0x10, ax, 0, 0, 0);
+      ax = 0xE * 256 + 0x8;
+      interrupt(0x10, ax, 0, 0, 0);
+    }
+  } else {
+    line[i] = temp;
+    i++;
+    ax = 0xE * 256 + temp;
+    interrupt(0x10, ax, 0, 0, 0);
+  }
+}
+}
+
+void readSector(char *buffer, int sector) {
+  char ah, al, ch, cl, dh, dl;
+  int ax, bx, cx, dx;
+
+  ah = 2;
+  al = 1;
+  bx = buffer;
+  ch = div(sector, 36);
+  cl = mod(sector, 18) + 1;
+  dh = mod(div(sector, 18), 2);
+  dl = 0;
+
+  ax = ah*256 + al;
+  cx = ch*256 + cl;
+  dx = dh*256 + dl;
+
+  interrupt(0x13, ax, bx, cx, dx);
+}
+
+int strnCmp(char *str1, char *str2, int length) {
+  int i, ret;
+
+  for (i = 0; i < length; i++) {
+    ret = str1[i] - str2[i];
+    if (ret != 0)
+      return ret;
+
+    if (str1[i] == 0)
+      return 0;
+  }
+  return 0;
+}
+
+int readFile(char *file, char *buffer) {
+  
+  char error[18];
+  char dir[512];
+  int entry, field, comp;
+  entry = 0;
+  field = 0;
+
+  readSector(dir, 2);
+
+  for (entry = 0; entry < 16; entry++) {
+    comp = strnCmp(file, dir + entry * 32, 6);
+    if (comp == 0) {
+      for (field = 6; field < 32; field++) {
+        comp = dir[entry * 32 + field];
+        if (comp == 0)
+          return 1;
+        readSector(buffer + 512 * (field - 6), comp);
+      }
+    }
+  }
+  error[0] = 'F';
+  error[1] = 'i';
+  error[2] = 'l';
+  error[3] = 'e';
+  error[4] = ' ';
+  error[5] = 'n';
+  error[6] = 'o';
+  error[7] = 't';
+  error[8] = ' ';
+  error[9] = 'f';
+  error[10] = 'o';
+  error[11] = 'u';
+  error[12] = 'n';
+  error[13] = 'd';
+  error[14] = '.';
+  error[15] = '\n';
+  error[16] = '\r';
+  error[17] = '\0';
+  printString(error);
+  return -1;
+}
+
+void executeProgram(char *name, int segment) {
+  char buffer[13312];
+  int i, result;
+
+  result = readFile(name, buffer);
+
+  if (result == -1){
+    return;
+  }
+
+  for (i = 0; i < 13312; i++) {
+    putInMemory(segment, i, buffer[i]);
+  }
+
+  launchProgram(segment);
+}
+
+void terminate() {
+  char str[6];
+  str[0] = 's';
+  str[1] = 'h';
+  str[2] = 'e';
+  str[3] = 'l';
+  str[4] = 'l';
+  str[5] = '\0';
+
+  interrupt(0x21, 4, str, 0x2000, 0);
+}
+
+void writeSector(char *name, int segment){
+  char ah, al, ch, cl, dh, dl;
+  int ax, bx, cx, dx;
+
+  ah = 3;
+  al = 1;
+  bx = name;
+  ch = div(segment, 36);
+  cl = mod(segment, 18) + 1;
+  dh = mod(div(segment, 18), 2);
+  dl = 0;
+
+  ax = ah*256 + al;
+  cx = ch*256 + cl;
+  dx = dh*256 + dl;
+
+  interrupt(0x13, ax, bx, cx, dx);
+}
+
+void deleteFile(char *name){
+  char error[18];
+  char dir[512];
+  char map[512];
+  int entry, field, comp;
+  entry = 0;
+  field = 0;
+
+  readSector(dir, 2);
+  readSector(map, 1);
+
+  for (entry = 0; entry < 16; entry++) {
+    comp = strnCmp(name, dir + entry * 32, 6);
+    if (comp == 0) {
+      for (field = 6; field < 32; field++) {
+        comp = dir[entry * 32 + field];
+        if (comp == 0)
+          break;
+        map[comp] = 0x0;
+      }
+      dir[entry * 32] = '\0';
+      break;
+    }
+    
+  }
+  writeSector(map, 1);
+  writeSector(dir, 2);
+}
---
+++ m4/shell.c
@@ -0,0 +1,85 @@
+/* Written by (Team 02) Chris Nurrenberg, Zac Davidsen, Trey Lewis 1/20/18*/
+
+#define PRINTSTRING 0x21, 0
+#define READSTRING 0x21, 1
+#define READSECTOR 0x21, 2
+#define READFILE 0x21, 3
+#define EXECUTEPROGRAM 0x21, 4
+#define TERMINATE 0x21, 5
+#define WRITESECTOR 0x21, 6
+#define DELETEFILE 0x21, 7
+
+int strnCmp(char *str1, char *str2, int length);
+void parseArguments(char *args, int *argc, char **argv);
+
+int main() {
+  char cmdBuf[80];
+  int i;
+  char buffer[13312];
+  int argCount;
+  char *argArray[5];
+
+  while (1) {
+    for (i = 0; i < 80; i++) {
+      cmdBuf[i] = 0;
+    }
+    for (i = 0; i < 13312; i++) {
+      buffer[i] = 0;
+    }
+
+    interrupt(PRINTSTRING, "SHELL: ", 0, 0);
+    interrupt(READSTRING, cmdBuf, 0, 0);
+
+    
+    parseArguments(cmdBuf, &argCount, argArray);
+
+    if (strnCmp(argArray[0], "type", 4) == 0 && argCount >= 2) {
+      interrupt(READFILE, argArray[1], buffer);
+      interrupt(PRINTSTRING, buffer, 0, 0);
+      interrupt(PRINTSTRING, "\n\r", 0, 0);
+    } else if (strnCmp(argArray[0], "execute", 7) == 0 && argCount >= 2) {
+      interrupt(EXECUTEPROGRAM, argArray[1], 0x3000);
+    } else if (strnCmp(argArray[0], "delete", 6) == 0) {
+      interrupt(DELETEFILE, argArray[1], 0, 0);
+    } else {
+      interrupt(PRINTSTRING, "Invalid Command\n\r", 0, 0);
+    }
+  }
+}
+
+int strnCmp(char *str1, char *str2, int length) {
+  int i, ret;
+
+  for (i = 0; i < length; i++) {
+    ret = str1[i] - str2[i];
+    if (ret != 0)
+      return ret;
+
+    if (str1[i] == 0)
+      return 0;
+  }
+  return 0;
+}
+
+void parseArguments(char *args, int *argc, char **argv){
+  int i, inArg;
+  *argc = 0;
+  inArg = 0;
+
+
+  i = 0;
+  while(args[i] != '\0') {
+    if (args[i] != ' '){
+      if (!inArg){
+        (argv)[*argc] = args + i;
+        (*argc)++;
+      }
+      inArg = 1;
+    }
+    else if (args[i] == ' '){
+      inArg = 0;
+      args[i] = 0x0;
+    }
+    i++;
+  }
+}
